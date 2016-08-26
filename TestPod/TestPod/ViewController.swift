990952bb21f7978473fcd1466c5357b345acf66a
卓同学 <lacklock@gmail.com>
2016-08-26 10:31:24 +0800

    add test pod project

---
+++ TestPod/TestPod/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  TestPod
+//
+//  Created by 卓同学 on 16/8/26.
+//  Copyright © 2016年 lacklock@gmail.com. All rights reserved.
+//
+
+import UIKit
+import ZHIconFontKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        IconFont.registerIconFontWithName("iconfont")
+    }
+
+
+}
+
